9fd602ee80b5142ed30d0ca0206d50e5719960fa
Suraj Barole <103420352+Surajbarole@users.noreply.github.com>
2023-10-08 10:42:28 +0530

    Update reducers.js

--- src/reducers.js
+++ src/reducers.js
@@ -1,5 +1,37 @@
 import { v4 as uuidv4 } from "uuid";
 
-const reducer = (state, action) => {};
+import { ADD_USER, HANDLE_FUEL } from './actionTypes';
 
-export default reducer;
+const initialState = {
+  users: [],
+  walletBalance: 0,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_USER:
+      const newUser = {
+        id: uuidv4(),
+        name: action.payload.name,
+      };
+      return {
+        ...state,
+        users: [...state.users, newUser],
+        walletBalance: state.walletBalance + 10,
+      };
+
+    case HANDLE_FUEL:
+      if (state.walletBalance >= 50) {
+        return {
+          ...state,
+          walletBalance: state.walletBalance - 50,
+        };
+      }
+      return state;
+
+    default:
+      return state;
+  }
+};
+
+export default reducer;
